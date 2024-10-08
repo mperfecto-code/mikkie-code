a0154c9b0d2d7d3119517a80f691148d5c1b8461
guibbz <guibilondo45@gmail.com>
2024-10-08 15:10:23 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Routes Here
+const authRoutes = require('./routes/authRoutes');
+const userRoute = require('./routes/userRoute');
+
+const app = express();
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', function(req, res) {
+    res.send('Kyle Guibilondo, MIT');
+});
+
+// Endpoint Here
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoute);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
